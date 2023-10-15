b053f381660954d981609925c97d41eb94a9629b
sugi04 <sugishivam8@gmail.com>
2023-10-15 18:24:00 +0530

    Cors origin

--- index.js
+++ index.js
@@ -8,21 +8,16 @@ const cookieParser = require("cookie-parser");
 
 const smtpPort = process.env.smtpPort || 25;
 const port = process.env.PORT;
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:800",
 
-  "https://gmail-clone-f6s7.onrender.com/"
-];
 app.use(express.json());
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "https://gmail-clone-f6s7.onrender.com/",
     credentials: true
   })
 );
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", allowedOrigins);
+  res.header("Access-Control-Allow-Origin", origin);
   res.header(
     "Access-Control-Allow-Methods",
     "GET,HEAD,OPTIONS,POST,PUT,DELETE"

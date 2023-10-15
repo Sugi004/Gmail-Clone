01806e435e921e3a57875ed0689fd9d2f859add3
sugi04 <sugishivam8@gmail.com>
2023-10-15 18:59:38 +0530

    Cors origin

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -17,15 +18,16 @@ app.use(
   })
 );
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+    "Access-Control-Allow-Origin: https://gmail-clone-fe--lambent-cascaron-86ce02.netlify.app, http://localhost:5173"
   );
+  res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Header",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  res.header("Access-Control-Allow-Credentials: true");
+  res.header("Access-Control-Allow-Headers: Content-Type, *");
   next();
 });
 app.use(cookieParser());

22b9ebb74ccb2330a8e7109c3f3ce0dfcb4c82ba
sugi04 <sugishivam8@gmail.com>
2023-10-15 18:42:27 +0530

    Cors origin

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://gmail-clone-fe--lambent-cascaron-86ce02.netlify.app/",
+    origin: "https://gmail-clone-f6s7.onrender.com/",
     credentials: true
   })
 );
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://gmail-clone-fe--lambent-cascaron-86ce02.netlify.app/"
+    "https://gmail-clone-f6s7.onrender.com/"
   );
   res.header(
     "Access-Control-Allow-Methods",

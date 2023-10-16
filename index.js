1c29559dd3ffe18baee27c2e9f055d6fb98c41a6
sugi04 <sugishivam8@gmail.com>
2023-10-16 11:15:50 +0530

    added cors

--- index.js
+++ index.js
@@ -12,19 +12,20 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://gmail-clone-fe--lambent-cascaron-86ce02.netlify.app"
-    ],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    preflightContinue: true,
-    accessControlMaxAge: 7200,
-    accessControlAllowHeaders: "Content-Type, *",
-    credentials: true
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://gmail-clone-fe--lambent-cascaron-86ce02.netlify.app"
+//     ],
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+//     preflightContinue: true,
+//     accessControlMaxAge: 7200,
+//     accessControlAllowHeaders: "Content-Type, *",
+//     credentials: true
+//   })
+// );
 
 app.use("/", routes);
 
